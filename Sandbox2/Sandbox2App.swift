cd920d4c8575a6d36ec9a46121229a7993fbfe7b
lizett <lizett503@gmail.com>
2022-02-01 15:13:26 -0800

    Initial Commit

---
+++ Sandbox2/Sandbox2App.swift
@@ -0,0 +1,17 @@
+//
+//  Sandbox2App.swift
+//  Sandbox2
+//
+//  Created by Susan Tovar on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Sandbox2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
